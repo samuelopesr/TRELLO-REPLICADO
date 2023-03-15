d01a17a854851e55a2481a93d4ce9414213da838
SaMucsPoP <116589388+SaMucsPoP@users.noreply.github.com>
2023-03-15 14:59:27 -0300

    att

--- JavaScript/pagina-trello.js
+++ JavaScript/pagina-trello.js
@@ -1,11 +1,11 @@
 function abrirMenu(){
     document.getElementById('menu-abrir-fechar').style.width = "250px";
     document.getElementById('main').style.marginLeft = "250px";
-    document.getElementById("btn-abrir").style.display = 'none';
+    document.getElementById("abrirMenu").style.display = 'none';
 }
 function fecharMenu(){
     document.getElementById('menu-abrir-fechar').style.width = "0px";
     document.getElementById('main').style.marginLeft = "0px";
-    document.getElementById("btn-abrir").style.display = 'block';
-    
+    document.getElementById("abrirMenu").style.display = 'block';
+
 }
