ac262af04f141fbb5944d89eb5ed075135bb600b
SaMucsPoP <116589388+SaMucsPoP@users.noreply.github.com>
2023-03-20 21:57:58 -0300

    att

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -37,9 +37,19 @@ window.addEventListener("resize", () => {
 });
 
 
+let quadros = [];
 
+function carregarQuadros() {
+  const quadrosJSON = localStorage.getItem("quadros");
+  if (quadrosJSON) {
+    quadros = JSON.parse(quadrosJSON);
+  }
+}
+
+carregarQuadros();
 
 let numQuadros = 0;
+let quadro = []
 
 function criarQuadro() {
 
@@ -53,18 +63,25 @@ function criarQuadro() {
   quadro.classList.add("quadro");
   quadro.style.width = "210px";
   quadro.style.height = "130px";
-  
+  const cor = gerarCorAleatoria();
+  quadro.style.backgroundColor = cor;
   
   const botaoExcluir = document.createElement("button");
   botaoExcluir.textContent = "Excluir quadro";
   botaoExcluir.addEventListener("click", () => {
     quadro.remove();
     numQuadros--;
+    const index = quadros.findIndex((q) => q.cor === cor);
+    quadros.splice(index, 1);
+    salvarQuadros();
   });
   
  
   const texto = document.createElement("h2");
   texto.textContent = "Novo quadro";
+  const titulo = prompt("Digite o título do quadro:");
+  if (!titulo) return;
+  texto.textContent = titulo;
   
 
   quadro.appendChild(botaoExcluir);
@@ -76,11 +93,137 @@ function criarQuadro() {
   
 
   numQuadros++;
+
+  const quadroInfo = {
+    cor: quadro.style.backgroundColor,
+    texto: texto.textContent,
+  };
+  quadro.push(quadroInfo);
+
+  const novoQuadro = {
+    titulo,
+    cor,
+  };
+
+  quadros.push(novoQuadro);
+  numQuadros++;
+
+  localStorage.setItem("quadros", JSON.stringify(quadro));
+
+  salvarQuadros();
+
 }
 
 
+
+function carregarQuadros() {
+
+    fetch('quadros.json')
+    .then(response => response.json())
+    .then(data => {
+      quadros = data;
+      numQuadros = quadros.length;
+      renderizarQuadros();
+    });
+    
+    const quadrosJSON = localStorage.getItem("quadros");
+    if (quadrosJSON) {
+      
+      quadros = JSON.parse(quadrosJSON);
+      quadros.forEach((quadroInfo) => {
+        const quadro = document.createElement("div");
+        quadro.classList.add("quadro");
+        quadro.style.width = "210px";
+        quadro.style.height = "130px";
+        quadro.style.backgroundColor = quadroInfo.cor;
+  
+        const botaoExcluir = document.createElement("button");
+        botaoExcluir.textContent = "Excluir quadro";
+        botaoExcluir.addEventListener("click", () => {
+          quadro.remove();
+          numQuadros--;
+          
+          quadro = quadro.filter((q) => q !== quadroInfo);
+          localStorage.setItem("quadros", JSON.stringify(quadro));
+        });
+  
+        const texto = document.createElement("h2");
+        texto.textContent = quadroInfo.texto;
+  
+        quadro.appendChild(botaoExcluir);
+        quadro.appendChild(texto);
+  
+        const container = document.querySelector("#criar-quadro");
+        container.appendChild(quadro);
+  
+        numQuadros++;
+      });
+    }
+  }
+
+  function renderizarQuadros() {
+    const container = document.querySelector("#criar-quadro");
+  
+    for (let i = 0; i < quadros.length; i++) {
+      const quadro = document.createElement("div");
+      quadro.classList.add("quadro");
+      quadro.style.width = "210px";
+      quadro.style.height = "130px";
+      quadro.style.backgroundColor = quadros[i].cor;
+  
+      const botaoExcluir = document.createElement("button");
+      botaoExcluir.textContent = "Excluir quadro";
+      botaoExcluir.addEventListener("click", () => {
+        quadro.remove();
+        numQuadros--;
+        quadros.splice(i, 1);
+        salvarQuadros();
+      });
+  
+      const texto = document.createElement("h2");
+      texto.textContent = quadros[i].titulo;
+  
+      quadro.appendChild(botaoExcluir);
+      quadro.appendChild(texto);
+  
+      container.appendChild(quadro);
+    }
+  }
+
+  function salvarQuadros() {
+    const json = JSON.stringify(quadros);
+    fetch('quadros.json', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: json
+    });
+  }
+
+function excluirTodosQuadros() {
+    if (confirm("Tem certeza que deseja excluir todos os quadros?")) {
+      const quadros = document.querySelectorAll(".quadro");
+      for (let i = 0; i < quadros.length; i++) {
+        quadros[i].remove();
+      }
+      numQuadros = 0;
+      quadros = [];
+      localStorage.removeItem("quadros")
+    }
+  }
+
+function gerarCorAleatoria() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+  }
+
 const botaoCriarQuadro = document.querySelector("#criar-novo-quadro");
 botaoCriarQuadro.addEventListener("click", criarQuadro);
+
+window.addEventListener("load", carregarQuadros);
 
 
 
